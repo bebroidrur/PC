de714b4495a40f88b90191f77a5f3bc26455efef
Marharyta Radchuk <ritoscocos1@gmail.com>
2026-04-20 12:27:04 +0300

    Lab5: implement basic TCP server with socket, bind, listen, accept

--- Lab5/serverlab5.cpp
+++ Lab5/serverlab5.cpp
@@ -1,3 +1,54 @@
-//
-// Created by mac on 20.04.2026.
-//
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <netinet/in.h>
+
+#define PORT 8080
+
+int main() {
+    int serverSocket;
+    struct sockaddr_in serverAddr;
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket < 0) {
+        std::cerr << "Socket creation failed\n";
+        return 1;
+    }
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        std::cerr << "Bind failed\n";
+        close(serverSocket);
+        return 1;
+    }
+
+    if (listen(serverSocket, 5) < 0) {
+        std::cerr << "Listen failed\n";
+        close(serverSocket);
+        return 1;
+    }
+
+    std::cout << "Server listening on port " << PORT << "...\n";
+
+    while (true) {
+        struct sockaddr_in clientAddr;
+        socklen_t clientLen = sizeof(clientAddr);
+
+        int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientLen);
+        if (clientSocket < 0) {
+            std::cerr << "Accept failed\n";
+            continue;
+        }
+
+        std::cout << "Client connected\n";
+
+        close(clientSocket);
+    }
+
+    close(serverSocket);
+    return 0;
+}
